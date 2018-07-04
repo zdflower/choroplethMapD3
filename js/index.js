c1079b1f9688c526712e709e822cc63c932e2edd
zdflower <zdflower@users.noreply.github.com>
2018-07-04 15:41:32 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,39 @@
-var height = 700;
-var width = 900;
+const height = 700;
+const width = 900;
 
-var svg = d3.select("body").append("svg").attr("width", width).attr("height", height).append("g").attr("transform", "translate(0,0)");
+const svg = d3.select("body").append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .append("g")
+  .attr("transform", "translate(0,0)");
 
-d3.queue().defer(d3.json, "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json").await(ready);
+// https://github.com/d3/d3-queue
+// http://giscollective.org/d3-queue-js/
+// https://bl.ocks.org/mbostock/4060606
+
+d3.queue()
+  .defer(d3.json, "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json")
+  .defer(d3.json, "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json")
+  .await(ready);
 
 var path = d3.geoPath();
 
-function ready(error, us) {
+function ready(error, counties, eduData) {
   if (error) throw error;
+  readyCounties(counties);
+  readyEduData(eduData);
+}
 
-  svg.append("g").attr("class", "counties").selectAll("path").data(topojson.feature(us, us.objects.counties).features).enter().append("path").attr("fill", "red").attr("d", path);
+function readyCounties(counties) {
+    svg.append("g")
+      .attr("class", "counties")
+      .selectAll("path")
+      .data(topojson.feature(counties, counties.objects.counties).features)
+        .enter().append("path")
+        .attr("fill", "red")
+        .attr("d", path)  
+}
+
+function readyEduData(eduData){
+  // 
 }
